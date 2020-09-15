4a460fbe5da0a44ca41f3c2db4c2ca533f01457e
Guilherme Soterio <guilhermehenrique.oliv3@gmail.com>
2020-09-14 22:38:06 -0300

    Entities, Repositories

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Genre.java
@@ -0,0 +1,80 @@
+package com.devsuperior.dspesquisa.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_Genre")
+public class Genre implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "genre")
+	private List<Game> games = new ArrayList<>();
+	
+	//Foi criado para ser chamado dentro de outras classes.
+	public Genre() {
+	}
+	
+	public Genre(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	public List<Game> getGames() {
+		return games;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Genre other = (Genre) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	
+}
