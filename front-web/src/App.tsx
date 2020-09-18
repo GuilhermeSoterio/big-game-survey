5ae1a956f00728f1f85c553dad445780d522e48d
Guilherme Soterio <guilhermehenrique.oliv3@gmail.com>
2020-09-18 19:27:48 -0300

    Adicionando componente header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1> Hello semana DevSuperior!</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
---
+++ front-web/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+
+const Header = () => (
+    <header className="main-header">
+        <Logo />
+        <div className="logo-text">
+            <span className="logo-text-1">Big Game</span>
+            <span className="logo-text-2">Survey</span>
+        </div>
+    </header>
+);
+
+export default Header;
