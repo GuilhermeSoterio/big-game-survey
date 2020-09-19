08a77af781038df46b113ff7ca9f9997fc95209f
Guilherme Soterio <guilhermehenrique.oliv3@gmail.com>
2020-09-19 05:39:01 -0300

    Criando pagina Home

---
+++ front-web/src/pages/Home/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './styles.css';
+import { ReactComponent as ArrowIcon } from '../../assets/Seta.svg';
+import { ReactComponent as GamerImage } from '../../assets/gamer.svg';
+
+const Home = () => (
+    <div className="home-container">
+        <div className="home-text">
+        <h1 className="home-text-title">
+            Quais jogos agalera gosta mais?
+        </h1>
+        <h3 className="home-text-subtitle">
+            Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!    
+        </h3>
+        <div className="home-actions">
+            <button className="home-btn">
+                QUERO SABER QUAIS SÃO
+            </button>
+            <div className="home-btn-icon">
+                <ArrowIcon />
+            </div>
+        </div>
+    </div>
+    <GamerImage className="home-image" />
+    </div>
+);
+
+export default Home;
