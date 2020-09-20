a1636f89a648197324975845023a31d440dd1eae
Guilherme Soterio <guilhermehenrique.oliv3@gmail.com>
2020-09-20 03:39:31 -0300

    Adicionando a página de gráficos

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Records from './pages/Records';
+import Charts from './pages/Charts';
 //É necessario o "exact pois quer dizer quando for exatamente o /, carrega aquilo"
 
 const Routes = () => (
@@ -14,6 +15,9 @@ const Routes = () => (
             </Route>
             <Route path="/records">
                 <Records />
+            </Route>
+            <Route path="/charts">
+                <Charts />
             </Route>
         </Switch>
     </BrowserRouter>
--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { RecordsResponse } from './types';
 import {  formatDate } from './helpers';
 import Pagination from './Pagination';
-import { Link } from 'react-router-dom';
+import Filters from '../../components/Filters';
 
 const BASE_URL = 'http://localhost:8080'
 
@@ -23,13 +23,7 @@ const Records = () => {
 
     return  (
        <div className="page-container">
-           <div className="filters-container records-actions">
-               <Link to="/charts">
-                   <button className="action-filters">
-                       VER GRÁFICOS
-                   </button>
-               </Link>
-           </div>
+           <Filters link="/charts" linkText="VER GRÁFICO"/>
            <table className="records-table" cellPadding="0" cellSpacing="0">
                 <thead>
                     <tr>
